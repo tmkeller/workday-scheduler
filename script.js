d3183541912216442b4d746d5f461201caa01a53
Tim Keller <timothy.m.keller@gmail.com>
2020-12-17 03:06:24 -0800

    Re-added new table rows and child elements. Finished styling them, including with cool extrusion effect through border-type: outset. Added logic for save button visual click effect.

--- script.js
+++ script.js
@@ -1,2 +1,18 @@
 var dayDate = moment().format('dddd, MMMM Do, YYYY');
 $( "#currentDay" ).text( dayDate );
+
+$( ".saveBtn" ).hover( function( event ) {
+    $( event.target ).children().first().attr( "fill", "black" );
+}, function( event ) {
+    $( event.target ).children().first().attr( "fill", "white" );
+});
+
+$( ".saveBtn" ).mousedown( function( event ) {
+    $( this ).css( "border-style", "inset" );
+    $( this ).css( "background-color", "#00657c" );
+});
+
+$( ".saveBtn" ).mouseup( function( event ) {
+    $( this ).css( "border-style", "outset" );
+    $( this ).css( "background-color", "#06AED5" );
+});
