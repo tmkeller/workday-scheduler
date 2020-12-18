51e6e159e1d525673fbf899a606943f155fb53de
Tim Keller <timothy.m.keller@gmail.com>
2020-12-17 19:37:26 -0800

    Added comments, switched function that adds past, present, and future classes to only target the text areas themselves, changed HTML to support it, and got rid of some excess whitespace.

--- script.js
+++ script.js
@@ -1,6 +1,9 @@
+// Get current hour of the day.
 var currentHour = parseInt( moment().format( 'HH' ) );
-var hourHeaders = $( ".hour" );
-
+// Get a JQuery object with all editable text areas.
+var textAreas = $( ".description" );
+// Add the current day to the header.
+$( "#currentDay" ).text( moment().format('dddd, MMMM Do, YYYY') );
 // Retrieve any stored data.
 for ( var i = 9; i <= 17; i++ ) {
     var sched = "hour-" + i;
@@ -9,36 +12,33 @@ for ( var i = 9; i <= 17; i++ ) {
         $( `#${ sched }` ).text( item );
     }
 }
-
-hourHeaders.each( function( index ) {
-    var hourIndex = $( hourHeaders[ index ] ).attr( "data-index");
+// Iterate over all elements in textAreas to determine whether their current time is in the past, present, or future.
+textAreas.each( function( index ) {
+    var hourIndex = $( textAreas[ index ] ).attr( "data-index" );
 
     if ( hourIndex > currentHour ) {
-        $( hourHeaders[ index ] ).next().addClass( "future" );
+        $( textAreas[ index ] ).addClass( "future" );
     } else if ( hourIndex < currentHour ) {
-        $( hourHeaders[ index ] ).next().addClass( "past" );
+        $( textAreas[ index ] ).addClass( "past" );
     } else {
-        $( hourHeaders[ index ] ).next().addClass( "present" );
+        $( textAreas[ index ] ).addClass( "present" );
     }
 });
-
+// Store the contents of a text area by clicking it's neighboring button.
 $( ".saveBtn" ).click( function() {
     localStorage.setItem( $( this ).prev().attr( "id" ), $( this ).prev().text() );
 })
-
-$( "#currentDay" ).text( moment().format('dddd, MMMM Do, YYYY') );
-
+// Save disc in the save button now turns black when mouse enters it's button, and turns white again when mouse leaves.
 $( ".saveBtn" ).hover( function( event ) {
     $( event.target ).children().first().attr( "fill", "black" );
 }, function( event ) {
     $( event.target ).children().first().attr( "fill", "white" );
 });
-
+// Add the cool clicking effect to mimic a real-life button press.
 $( ".saveBtn" ).mousedown( function( event ) {
     $( this ).css( "border-style", "inset" );
     $( this ).css( "background-color", "#00657c" );
 });
-
 $( ".saveBtn" ).mouseup( function( event ) {
     $( this ).css( "border-style", "outset" );
     $( this ).css( "background-color", "#06AED5" );
